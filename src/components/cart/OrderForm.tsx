2d8eac967de0673819aab2fd8a8dffe298356bc2
NZ <ns@zav.me>
2023-01-09 13:38:45 +0300

    added inputs for address to the order form

--- src/components/cart/OrderForm.tsx
+++ src/components/cart/OrderForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
     Button,
+    Flex,
     FormControl,
     Input,
     InputGroup,
@@ -16,12 +17,15 @@ import {Field, Form, Formik, FormikHelpers} from "formik";
 import * as Yup from "yup";
 import {useCart} from "../../context/CartContext";
 import {useCustomer} from "../../context/CustomerContext";
-import {ToastSuccess} from "../../utilities/error-handling";
+import {ToastInfo} from "../../utilities/error-handling";
 
 interface Values {
     firstname: string;
     lastname: string;
-    address: string;
+    zipcode: string;
+    country: string;
+    city: string;
+    street: string;
     email: string;
     phone: string;
     comment: string;
@@ -38,8 +42,13 @@ export const OrderForm = () => {
         lastname: Yup.string()
             .max(70, 'Пожалуйста, введите не более 70 символов')
             .required('Пожалуйста, заполните обязательное поле'),
-        address: Yup.string()
-            .max(70, 'Пожалуйста, введите не более 200 символов')
+        zipcode: Yup.string()
+            .required('Пожалуйста, заполните обязательное поле'),
+        country: Yup.string()
+            .required('Пожалуйста, заполните обязательное поле'),
+        city: Yup.string()
+            .required('Пожалуйста, заполните обязательное поле'),
+        street: Yup.string()
             .required('Пожалуйста, заполните обязательное поле'),
         email: Yup.string()
             .email('Введен некорректный email')
@@ -53,7 +62,10 @@ export const OrderForm = () => {
             initialValues={{
                 firstname: customer?.firstname ?? '',
                 lastname: customer?.lastname ?? '',
-                address: '',
+                zipcode: customer?.address?.zipcode ?? '',
+                country: customer?.address?.country ?? '',
+                city: customer?.address?.city ?? '',
+                street: customer?.address?.street ?? '',
                 email: customer?.email ?? '',
                 phone: '',
                 comment: '',
@@ -68,74 +80,149 @@ export const OrderForm = () => {
                     console.log(order);
                     emptyCart();
                     setSubmitting(false);
-                    ToastSuccess('Спасибо за заказ. В ближайшее время наши операторы свяжутся с вами для уточнения сроков доставки.')
+                    console.log(order);
+                    ToastInfo('not implemented')
+                    // ToastSuccess('Спасибо за заказ. В ближайшее время наши операторы свяжутся с вами для уточнения сроков доставки.')
                 }, 1000)
             }}
         >
             {({isSubmitting, isValid, dirty}) => (
                     <Form>
-                        <VStack spacing={3} px={1} alignItems='start'>
-                            <FormControl id="firstname">
-                                <Field name="firstname">
-                                    {({field, meta}: any) => (
-                                        <>
-                                            <InputGroup>
-                                                <InputLeftElement
-                                                    pointerEvents="none"
-                                                    children={<BsPerson color="gray.800"/>}
-                                                />
-                                                <Input type="text"
-                                                       placeholder="Введите Ваше имя..."
-                                                       isInvalid={meta.touched ? meta.error : false} {...field} />
-                                            </InputGroup>
-                                            {meta.touched && meta.error && (
-                                                <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
-                                            )}
-                                        </>
-                                    )}
-                                </Field>
-                            </FormControl>
-                            <FormControl id="lastname">
-                                <Field name="lastname">
-                                    {({field, meta}: any) => (
-                                        <>
-                                            <InputGroup>
-                                                <InputLeftElement
-                                                    pointerEvents="none"
-                                                    children={<BsPerson color="gray.800"/>}
-                                                />
-                                                <Input type="text"
-                                                       placeholder="Введите Вашу фамилию..."
-                                                       isInvalid={meta.touched ? meta.error : false} {...field} />
-                                            </InputGroup>
-                                            {meta.touched && meta.error && (
-                                                <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
-                                            )}
-                                        </>
-                                    )}
-                                </Field>
-                            </FormControl>
-                            <FormControl id="address">
-                                <Field name="address">
-                                    {({field, meta}: any) => (
-                                        <>
-                                            <InputGroup>
-                                                <InputLeftElement
-                                                    pointerEvents="none"
-                                                    children={<BiHomeAlt color="gray.800"/>}
-                                                />
-                                                <Input type="text"
-                                                       placeholder="Город, улица, номер дома, номер квартиры"
-                                                       isInvalid={meta.touched ? meta.error : false} {...field} />
+                        <VStack spacing={3} px={1} align='stretch' justifyContent='space-between'>
+                            <Text fontWeight='bold' mt={3}>Имя и фамилия</Text>
+                            <Flex gap={3}>
+                                <FormControl id="firstname">
+                                    <Field name="firstname">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BsPerson color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Введите Ваше имя..."
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                                <FormControl id="lastname">
+                                    <Field name="lastname">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BsPerson color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Введите Вашу фамилию..."
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                            </Flex>
+                            <Text fontWeight='bold' mt={3}>Адрес</Text>
+                            <Flex gap={3}>
+                                <FormControl id="zipcode">
+                                    <Field name="zipcode">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BiHomeAlt color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Индекс"
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
 
-                                            </InputGroup>
-                                            {meta.touched && meta.error && (
-                                                <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
-                                            )}
-                                        </>
-                                    )}
-                                </Field>
-                            </FormControl>
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                                <FormControl id="country">
+                                    <Field name="country">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BiHomeAlt color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Страна"
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
+
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                            </Flex>
+
+                            <Flex gap={3}>
+                                <FormControl id="city">
+                                    <Field name="city">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BiHomeAlt color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Город"
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
+
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                                <FormControl id="street">
+                                    <Field name="street">
+                                        {({field, meta}: any) => (
+                                            <>
+                                                <InputGroup>
+                                                    <InputLeftElement
+                                                        pointerEvents="none"
+                                                        children={<BiHomeAlt color="gray.800"/>}
+                                                    />
+                                                    <Input type="text"
+                                                           placeholder="Улица, дом, квартира"
+                                                           isInvalid={meta.touched ? meta.error : false} {...field} />
+
+                                                </InputGroup>
+                                                {meta.touched && meta.error && (
+                                                    <Text color='red.400' fontSize='sm' mt={1}>{meta.error}</Text>
+                                                )}
+                                            </>
+                                        )}
+                                    </Field>
+                                </FormControl>
+                            </Flex>
+                            <Text fontWeight='bold' mt={3}>Контакты</Text>
                             <FormControl id="email">
                                 <Field name="email">
                                     {({field, meta}: any) => (
